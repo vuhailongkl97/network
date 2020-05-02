0e9a8fa4fc03e030fa252034ad91ff741f7ee97e
longkl <longkl@gmail.com>
2020-05-03 00:26:23 +0700

    add tcp application & dual stack & simple get time application

---
+++ tcp/server_tcp.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <time.h>
+
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <string.h>
+#include <error.h>
+#include <errno.h>
+
+int main(void)
+{
+    time_t timer;
+    int fd = 0;
+    struct sockaddr_in addr;
+
+    time(&timer);
+    char *time_msg = ctime(&timer);
+    printf("local time is: %s\n", ctime(&timer));
+
+    char  address_buffer[100];
+    static const uint8_t myaddr[16] = {0xfe, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00 , 0x00, \
+        0xb5, 0x82, 0xec, 0x91, 0x81, 0x03, 0x59, 0x09};
+    struct sockaddr_in6 server ;
+    server.sin6_family = AF_INET6;
+    server.sin6_port =   htons(8080);
+    //server.sin6_addr.s_addr = inet_addr("192.168.0.97");
+    //memcpy(server.sin6_addr.s6_addr, myaddr, sizeof myaddr);
+    server.sin6_addr = in6addr_any;
+
+    fd = socket(AF_INET6, SOCK_STREAM, 0);
+    if (fd < 0)
+    {
+        fprintf(stderr, "%s:%d error %s\n", __func__, __LINE__, strerror(errno));
+        return -1;
+    }
+
+    int true = 1;
+    if(setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &true, sizeof(int)) < 0)
+    {
+        fprintf(stderr, "%s:%d setsockopt error %s\n", __func__, __LINE__, strerror(errno));
+        return -1;
+    }
+
+    int option = 0;
+    if(setsockopt(fd, IPPROTO_IPV6, IPV6_V6ONLY, (void*)&option, sizeof(option)) < 0)
+    {
+        fprintf(stderr, "%s:%d setsockopt error %s\n", __func__, __LINE__, strerror(errno));
+        return -1;
+    }
+
+
+    if(bind(fd, (struct sockaddr *)&server, sizeof(server)) < 0)
+    {
+        fprintf(stderr, "%s:%d error %s\n", __func__, __LINE__, strerror(errno));
+        return -1;
+    }
+
+    if (listen(fd, 10) < 0)
+    {
+        fprintf(stderr, "%s:%d listen() error %s\n", __func__, __LINE__, strerror(errno));
+        return -1;
+    }
+
+    printf("Waiting for connection ...\n");
+
+    struct sockaddr_storage client_address;
+    socklen_t client_len = sizeof(client_address);
+
+    int new_fd = accept(fd, (struct sockaddr*)&client_address, &client_len);
+
+    if ( new_fd < 0)
+    {
+        fprintf(stderr, "%s:%d accept error %s\n", __func__, __LINE__, strerror(errno));
+        return -1;
+    }
+
+    puts("someone connected ..");
+    getnameinfo((struct sockaddr*)&client_address, client_len , address_buffer, \
+            sizeof(address_buffer), 0, 0 , NI_NUMERICHOST);
+
+    printf("%s\n", address_buffer);
+
+    char request[1024];
+    int bytes_received = recv(new_fd, request, 1024, 0);
+    printf("Recv %d bytes. request %.*s\n", bytes_received, bytes_received, request);
+    int bytes_send;
+    const char *response =
+        "HTTP/1.1 200 OK\r\n"
+        "Connection: close\r\n"
+        "Content-Type: text/plain\r\n\r\n";
+        //"Local time is : ";
+    bytes_send = send(new_fd, response, strlen(response), 0);
+    printf("Sent response %d of %d bytes\n", bytes_send , strlen(time_msg));
+
+    bytes_send = send(new_fd, time_msg, strlen(time_msg), 0);
+    printf("Sent %d of %d bytes\n", bytes_send , strlen(time_msg));
+
+    close(new_fd);
+    close(fd);
+
+    return 0;
+}
